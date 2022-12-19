52a98d2d1f0eed3ecc21774097fbbb4d3e64cb5b
Krishankb <30771097+Krishankb@users.noreply.github.com>
2022-12-19 14:56:39 +0530

    Create Calculator.java

---
+++ Beginner/Calculator.java
@@ -0,0 +1,46 @@
+public class Calculator{
+
+  public int add(int a,int b) {
+    int res;
+    res = a + b;
+    return res;
+  }
+
+  public int subtract(int a,int b) {
+      int res;
+      res = a - b;
+      return res;
+    }
+
+  public int multiply(int a,int b) {
+    int res;
+    res = a*b;
+    return res;
+  }
+
+  public int divide(int a, int b) {
+    int res;
+    res = a/b;
+    return res;
+  }
+
+  public int modulo(int a,int b) {
+    int res;
+    res = a % b;
+    return res;
+  }
+
+  public static void main(String[] args) {
+    Calculator myCalculator = new Calculator();
+    int addres,subres;
+    addres = myCalculator.add(5,7);
+    subres = myCalculator.subtract(45,11);
+    System.out.println(addres);
+    System.out.println(subres);
+    
+
+  }
+  
+
+}
+
