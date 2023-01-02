51abd28fbbe2e7bf8d726ed63ef1df578b352fd9
Krishankb <30771097+Krishankb@users.noreply.github.com>
2023-01-02 10:58:41 +0530

    Update Main.java

--- AWSDynamoDB/Main.java
+++ AWSDynamoDB/Main.java
@@ -2,10 +2,8 @@ package org.example;
 
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
-import com.amazonaws.services.dynamodbv2.document.DynamoDB;
-import com.amazonaws.services.dynamodbv2.document.Item;
-import com.amazonaws.services.dynamodbv2.document.Table;
-import com.amazonaws.services.dynamodbv2.document.UpdateItemOutcome;
+import com.amazonaws.services.dynamodbv2.document.*;
+import com.amazonaws.services.dynamodbv2.document.spec.DeleteItemSpec;
 import com.amazonaws.services.dynamodbv2.document.spec.UpdateItemSpec;
 import com.amazonaws.services.dynamodbv2.document.utils.NameMap;
 import com.amazonaws.services.dynamodbv2.document.utils.ValueMap;
@@ -35,27 +33,78 @@ public class Main {
     }
 
     private static void deleteItem() {
+        Table table = dynamoDB.getTable(tableName);
+        try {
+            DeleteItemSpec deleteItemSpec = new DeleteItemSpec()
+                    .withPrimaryKey("Id",120)
+                    .withConditionExpression("#ip = val")
+                    .withNameMap(new NameMap().with("#ip","InPublication"))
+                    .withValueMap(new ValueMap().withBoolean(":val",false)).withReturnValues(ReturnValue.ALL_OLD);
+
+            DeleteItemOutcome outcome = table.deleteItem(deleteItemSpec);
+
+            System.out.println("Printing item that was deleted....");
+            System.out.println(outcome.getItem().toJSONPretty());
+
+        } catch (Exception e) {
+            System.err.println("Delete item failed");
+            System.err.println(e.getMessage());
+//            throw new RuntimeException(e);
+        }
     }
 
     private static void updateExistingAttributeConditionally() {
+        try {
+            Table table = dynamoDB.getTable(tableName);
+            UpdateItemSpec updateItemSpec = new UpdateItemSpec()
+                    .withPrimaryKey("Id", 120)
+                    .withReturnValues(ReturnValue.ALL_NEW)
+                    .withConditionExpression("#p =: val2").withNameMap(new NameMap().with("#p", "price"))
+                    .withValueMap(new ValueMap().withNumber(":val1", 25).withNumber("val2", 20));
+            UpdateItemOutcome outcome = table.updateItem(updateItemSpec);
+        } catch (Exception e) {
+            System.err.println("Error updating ");
+            System.err.println(e.getMessage());
+//            throw new RuntimeException(e);
+        }
+
     }
 
     private static void updateAddNewAttribute() {
-    }
-
-    private static void updateMultipleAttributes() {
         Table table = dynamoDB.getTable(tableName);
         try {
-            UpdateItemSpec updateItemSpec = new UpdateItemSpec().withPrimaryKey("Id",121)
+            UpdateItemSpec updateItemSpec = new UpdateItemSpec()
+                    .withPrimaryKey("Id",121)
                     .withUpdateExpression("set #na =: val1")
                     .withNameMap(new NameMap().with("#na","NewAttribute"))
-                    .withValueMap(new ValueMap().withString(":val1", "Some value")).withReturnValues(ReturnValue.ALL_NEW);
+                    .withValueMap(new ValueMap().withString(":val1", "Some value"))
+                    .withReturnValues(ReturnValue.ALL_NEW);
 
             UpdateItemOutcome outcome = table.updateItem(updateItemSpec);
             System.out.println("printing item after updating");
             System.out.println(outcome.getItem().toJSONPretty());
         } catch (Exception e) {
             System.err.println("Failed to update the items");
+            System.err.println(e.getMessage());
+//            throw new RuntimeException(e);
+        }
+    }
+
+    private static void updateMultipleAttributes() {
+        Table table = dynamoDB.getTable(tableName);
+        try{
+            UpdateItemSpec updateItemSpec = new UpdateItemSpec()
+                    .withPrimaryKey("Id",120)
+                    .withUpdateExpression("add #a :val1 set #na=:val2")
+                    .withNameMap(new NameMap().with("#a", "Authors").with("#na","NewAttribute"))
+                    .withValueMap(new ValueMap().withStringSet(":val1","Author ZZ").withString(":val2","someValue"))
+                    .withReturnValues(ReturnValue.ALL_NEW);
+            UpdateItemOutcome outcome = table.updateItem(updateItemSpec);
+
+            System.out.println("Printing item after multiple enteries");
+            System.out.println(outcome.getItem().toJSONPretty());
+        } catch (Exception e) {
+            System.err.println("GetItem failed");
             System.err.println(e.getMessage());
 //            throw new RuntimeException(e);
         }
@@ -104,164 +153,3 @@ public class Main {
 
     }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
